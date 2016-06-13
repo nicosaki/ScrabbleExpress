8bf8a7597ce79af427f8a56f589a641520669218
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-13 15:53:45 -0700

    Changed router.gets to reference Controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Controller = require('../controllers/scoring')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', Controller.index)
+
+router.get('/scrabble/chart', Controller.chart)
+
+router.get('/scrabble/score', Controller.score)
+
+router.post('/scrabble/score', Controller.score_word)
+
+router.get('/scrabble/score/:word', Controller.value)
 
 module.exports = router;
+
+// GET /scrabble/chart: shows the letter score chart
+// GET /scrabble/score: shows a form allowing a user to submit a word for scoring
+// POST /scrabble/score: processes the user input and renders either
+// a page showing the user submitted word and it's score or
+// in the case of something it can't score, an error page with an "unscorable word" message
+// GET /scrabble/score/:name: A dynamic route that scores whatever word is passed into the :name portion of the dynamic route. Will render either
+// a page showing the user submitted word and it's score or
+// in the case of something it can't score, an error page with an "unscorable word" message
